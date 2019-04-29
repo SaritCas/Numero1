22a42b548a63cd05c168a0b03fec53b3c09dc84b
DaniHS <danielhachac@gmail.com>
2019-04-29 01:59:30 -0500

    13.2

--- ProyectoProMasterHack/conexion.php
+++ ProyectoProMasterHack/conexion.php
@@ -1,7 +1,7 @@
 <?php
   $db_host="localhost";
-  $db_name="id9433736_elefanteform";
-  $db_login="id9433736_root";
+  $db_name="id9446395_as";
+  $db_login="id9446395_root";
   $db_pswd="Lucky258";
   $db_table_name1="mensajes";
   $con=mysqli_connect($db_host,$db_login,$db_pswd,$db_name);
